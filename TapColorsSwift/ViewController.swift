5c4bb976b07d4d931764ecf1dc802ea7100052a9
ken0nek <ken.for.develop@gmail.com>
2015-12-02 19:49:46 +0900

    Update swift2

--- TapColorsSwift/ViewController.swift
+++ TapColorsSwift/ViewController.swift
@@ -11,40 +11,35 @@ import QuartzCore
 
 class ViewController: UIViewController {
     
-    var colorCombination: UIColor[] = UIColor[]()
-    let colorPalletCombination: UIColor[] = [UIColor.redColor(), UIColor.blueColor(), UIColor.greenColor(), UIColor.yellowColor()]
-    var position: Int = 0
-    
+    private var colorCombination = [UIColor]()
+    private let colorPalletCombination: [UIColor] = [.redColor(), .blueColor(), .greenColor(), .yellowColor()]
+    private var position: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.newGame()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        newGame()
     }
     
     // go next problem
-    func newGame() {
+    private func newGame() {
         position = 0
-        colorCombination = self.produceColorCombination()
-        self.makeLabelsFromArray(colorCombination)
+        colorCombination = produceColorCombination()
+        makeLabelsFromArray(colorCombination)
     }
     
-    func makeLabelsFromArray(array: Array<UIColor>) {
+    private func makeLabelsFromArray(array: [UIColor]) {
         var x: CGFloat = 40
-        var y: CGFloat = 60
-        for var i: Int = 0; i < array.count; i++ {
+        let xSkip: CGFloat = 65
+        let y: CGFloat = 60
+        for (i, color) in array.enumerate() {
             let rect = CGRectMake(x, y, 60, 60);
-            var label = UILabel(frame: rect)
+            let label = UILabel(frame: rect)
             label.tag = i + 101
-            label.backgroundColor = array[i]
-            self.view.addSubview(label)
-            x += 60 + 5
+            label.backgroundColor = color
+            view.addSubview(label)
+            x += xSkip
             if i == 0 {
                 label.layer.borderColor = UIColor.blackColor().CGColor
                 label.layer.borderWidth = 5.0
@@ -52,36 +47,35 @@ class ViewController: UIViewController {
         }
     }
     
-    func produceColorCombination() -> UIColor[] {
+    private func produceColorCombination() -> [UIColor] {
         
-        colorCombination = [UIColor(), UIColor(), UIColor(), UIColor()]
-        
-        for var i: Int = 0; i < 4; i++ {
-            colorCombination[i] = colorPalletCombination[Int(arc4random_uniform(UInt32(4)))]
-        }
-        
-        return colorCombination;
+        return Array(0 ..< 4).map {_ in colorPalletCombination[Int(arc4random_uniform(UInt32(4)))]}
     }
     
-    @IBAction func buttonPressed(button: UIButton) {
+    @IBAction private func buttonPressed(button: UIButton) {
         // check color
         if colorCombination[position] == button.backgroundColor {
-            let label: UILabel = self.view.viewWithTag(position + 101) as UILabel
+            let label = view.viewWithTag(position + 101) as! UILabel
             label.removeFromSuperview()
             
-            if position < 3 {
+            position++
+            
+            if position < 4 {
                 
-                let targetLabel: UILabel = self.view.viewWithTag(position + 102) as UILabel
+                let targetLabel = view.viewWithTag(position + 101) as! UILabel
                 targetLabel.layer.borderColor = UIColor.blackColor().CGColor
                 targetLabel.layer.borderWidth = 5.0
             }
-            
-            position++
         }
         
         // if combination becomes empty, go next game
         if position == 4 {
-            self.newGame()
+            newGame()
         }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 }
